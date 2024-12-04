afab4cfd5229719c8695926e3b83ac0ae5f9533e
myxlaxll <85912499+myxlaxll@users.noreply.github.com>
2024-12-04 15:00:17 +0800

    更新 ddns_script.sh

--- trunk/user/aliddns/ddns_script.sh
+++ trunk/user/aliddns/ddns_script.sh
@@ -1,18 +1,11 @@
 # 自行测试哪个代码能获取正确的IP，删除前面的#可生效
 arIpAddress () {
 # IPv4地址获取（基本无用，基本没有公网IPv4地址）
-# 获得外网地址
-curltest=`which curl`
-if [ -z "$curltest" ] || [ ! -s "`which curl`" ] ; then
-    wget --no-check-certificate --quiet --output-document=- "http://members.3322.org/dyndns/getip" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
-else
-    curl -L -k -s "http://members.3322.org/dyndns/getip" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
-fi
 }
 arIpAddress6 () {
 # IPv6地址获取
 # 因为一般ipv6没有nat ipv6的获得可以本机获得
-ifconfig $(nvram get wan0_ifname_t) | awk '/Global/{print $3}' | awk -F/ '{print $1}'
+ifconfig $(nvram get lan0_ifname_t) | awk '/Global/{print $3}' | awk -F/ '{print $1}'
 #curl -6 -L --user-agent "$user_agent" -s "https://[2606:4700:4700::1002]/cdn-cgi/trace" | awk -F= '/ip/{print $2}'
 #ip -6 neigh show | grep 'MAC地址' | grep '24' | grep -o "^\S\+" | head -n 1
 #网络邻居显示           包含'MAC地址'          包含 '24'开头        去掉后面        第一行
