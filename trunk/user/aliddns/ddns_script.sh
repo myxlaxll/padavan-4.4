cb2c2ff93e464e3a646c4e863038574d34288e59
myxlaxll <85912499+myxlaxll@users.noreply.github.com>
2024-12-04 19:19:23 +0800

    更新 ddns_script.sh

--- trunk/user/aliddns/ddns_script.sh
+++ trunk/user/aliddns/ddns_script.sh
@@ -1,30 +1,46 @@
 # 自行测试哪个代码能获取正确的IP，删除前面的#可生效
 arIpAddress () {
-# IPv4地址获取
+# IPv4地址获取(几乎无用)
 # 获得外网地址
+pppoemwan=`nvram get pppoemwan_enable`
+if [ "$pppoemwan" -ne 0 ]; then
+        curl --interface ppp0 -L -k -s "http://members.3322.org/dyndns/getip" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
+else
 curltest=`which curl`
 if [ -z "$curltest" ] || [ ! -s "`which curl`" ] ; then
-    #wget --no-check-certificate --quiet --output-document=- "https://www.ipip.net" | grep "IP地址" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
-    wget --no-check-certificate --quiet --output-document=- "http://members.3322.org/dyndns/getip" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
-    #wget --no-check-certificate --quiet --output-document=- "ip.6655.com/ip.aspx" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
-    #wget --no-check-certificate --quiet --output-document=- "ip.3322.net" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
+    wget -T 5 -t 3 --no-check-certificate --quiet --output-document=- "http://members.3322.org/dyndns/getip" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
 else
-    #curl -L -k -s "https://www.ipip.net" | grep "IP地址" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
     curl -L -k -s "http://members.3322.org/dyndns/getip" | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
-    #curl -L -k -s ip.6655.com/ip.aspx | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
-    #curl -L -k -s ip.3322.net | grep -E -o '([0-9]+\.){3}[0-9]+' | head -n1 | cut -d' ' -f1
+fi
 fi
 }
 arIpAddress6 () {
 # IPv6地址获取
-
+# 因为一般ipv6没有nat ipv6的获得可以本机获得
 ifconfig $(nvram get lan0_ifname_t) | awk '/Global/{print $3}' | awk -F/ '{print $1}'
-#ifconfig br0 | awk '/Global/{print $3}' | awk -F/ '{print $1}'
 #ip -6 neigh show | grep 'MAC' | grep '24' | grep -o "^\S\+" | head -n 1
-#网络邻居显示   	包含'MAC地址'  	包含 '24'开头	去掉后面	    第一行
+#网络邻居显示   	'MAC地址'  	 '2408等前缀'	去掉后面	第一行
 }
+if [ "$IPv6_neighbor" != "1" ] ; then
 if [ "$IPv6" = "1" ] ; then
 arIpAddress=$(arIpAddress6)
 else
 arIpAddress=$(arIpAddress)
 fi
+else
+arIpAddress=$inet6_neighbor
+inet6_neighbor=""
+IPv6_neighbor=0
+fi
+
+# 根据 ip -6 neigh show 获取终端的信息，设置 ddns 解析，实现每个终端的 IPV6 动态域名
+# 参数说明：使用 @ 符号分割，①前缀名称 ②域名 ③MAC【不限大小写】
+# ④匹配关键词的ip6地址【可留空/2408等前缀】 ⑤排除关键词的ip6地址【可留空】 ⑥手动指定ip【可留空】 
+# 下面是信号填写例子：（删除前面的#可生效）
+cat >/tmp/ip6_ddns.inf <<-\EOF
+
+#www@google.com@09:9B:9A:90:9F:D9@@fe80::@
+
+EOF
+cat /tmp/ip6_ddns.inf | grep -v '^#'  | grep -v '^$' > /tmp/ip6_ddns_inf
+rm -f /tmp/ip6_ddns.inf
