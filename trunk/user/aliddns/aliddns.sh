a3d62b0cad618ba02812e96a9107565e7dfab8e3
myxlaxll <85912499+myxlaxll@users.noreply.github.com>
2024-12-05 17:11:48 +0800

    Update aliddns.sh

--- trunk/user/aliddns/aliddns.sh
+++ trunk/user/aliddns/aliddns.sh
@@ -166,16 +166,6 @@ kill_ps "$scriptname"
 aliddns_start () {
 IPv6=0
 
-a_ip6=/tmp/ip6_neighbor.log
-b_ip6=/tmp/ip6_neighbor_addr.log
-c_ip6=/tmp/static_ip6.inf
-touch $a_ip6 $b_ip6 $c_ip6
-neighbors=$(ip -f inet6 neighbor show)
-echo $neighbors > $a_ip6
-sed -i 's/ STALE /\n/g' /tmp/ip6_neighbor.log
-sed -i 's/ REACHABLE /\n/g' /tmp/ip6_neighbor.log
-sed -i 's/ FAILED /\n/g' /tmp/ip6_neighbor.log
-
 if [ "$aliddns_domain"x != "x" ] && [ "$aliddns_name"x != "x" ] ; then
 	sleep 1
 	timestamp=`date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ"`
@@ -223,12 +213,24 @@ do
 	inet6_neighbor="$(echo "$line" | cut -d '@' -f6)"
 	inet6_neighbor=$(echo $inet6_neighbor)
 	if [ -z "$inet6_neighbor" ] ; then
-			
-			ip6_addrget="$(echo "$a_ip6" | grep -i '$inf_MAC' | grep -i '$inf_match' | grep -v '$inf_v_match' | grep -o "^\S\+" | head -n 1)"
-   			
-			echo "$ip6_addrget" >> $c_ip6
-			echo "$ip6_addrget" > $b_ip6
-			inet6_neighbor=$ip6_addrget
+		a_ip6=/tmp/ip6_neighbor.log
+		b_ip6=/tmp/ip6_neighbor_addr.log
+		c_ip6=/tmp/static_ip6.inf
+		touch $a_ip6 $b_ip6 $c_ip6
+		neighbors=$(ip -f inet6 neighbor show)
+		echo $neighbors > $a_ip6
+		sed -i 's/ STALE /\n/g' /tmp/ip6_neighbor.log
+		sed -i 's/ REACHABLE /\n/g' /tmp/ip6_neighbor.log
+		sed -i 's/ FAILED /\n/g' /tmp/ip6_neighbor.log
+		#切割完成，IPv6_MAC绑定分行显示
+		cat /tmp/ip6_neighbor.log | grep -v ''$inf_v_match'' | grep -i ''$inf_MAC'' > /tmp/ip6_neighbor_addr.log
+		#去掉内网, 覆盖写入/tmp/ip6_neighbor_addr.log文件内
+		#删除需要排除的MAC地址的行，留下外网临时IPv6-MAC绑定地址
+		ip6_addrget="$(cat /tmp/ip6_neighbor_addr.log | cut -d ' ' -f1 | head -n 1) "
+		#取得第一个空格前的数据-IPv6地址
+		echo "$ip6_addrget" >> $c_ip6
+		echo "$ip6_addrget" > $b_ip6
+		inet6_neighbor=$ip6_addrget
 	fi
 	[ ! -z "$inet6_neighbor" ] && arDdnsCheck $domain $name
 	IPv6_neighbor=0
