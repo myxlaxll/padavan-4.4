bdb468e381bde82d174b378b53dbf87382f2b403
myxlaxll <85912499+myxlaxll@users.noreply.github.com>
2024-12-10 00:13:35 +0800

    Update aliddns.sh

--- trunk/user/aliddns/aliddns.sh
+++ trunk/user/aliddns/aliddns.sh
@@ -168,8 +168,7 @@ IPv6=0
 ip -f inet6 neighbor flush all
 #清理邻居表缓存，防止出现过期IPv6地址
 sleep 10
-ip -f inet6 neighbor show
-sleep 10
+#ip -f inet6 neighbor show
 if [ "$aliddns_domain"x != "x" ] && [ "$aliddns_name"x != "x" ] ; then
 	sleep 1
 	timestamp=`date -u "+%Y-%m-%dT%H%%3A%M%%3A%SZ"`
